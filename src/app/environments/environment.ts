9736f01add1f1dc17ba09ba8fc51b1c0a3092f57
Alex Broussard <alex.broussard@itesoft.com>
2024-12-14 22:34:56 +0100

    Empty environment files since I will regenerate secrets on strava and also generate env files automatically

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,8 +1,3 @@
 export const environment = {
-  production: false,
-  stravaConfig: {
-    clientId: '140769',
-    clientSecret: 'df294efa81031b09edefc88265af729717c2a6b6',
-    redirectUri: 'https://localhost:4200/callback'
-  }
+  production: false
 };
