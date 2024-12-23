4cb06ff709c56218770850199798bf3ce0e03656
Alex Broussard <alex.broussard@itesoft.com>
2024-12-23 12:04:15 +0100

    Improve period-selector

--- src/app/components/period-selector/period-selector.component.ts
+++ src/app/components/period-selector/period-selector.component.ts
@@ -1,5 +1,4 @@
-// src/app/components/period-selector/period-selector.component.ts
-import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {Component, ElementRef, EventEmitter, HostListener, Input, Output} from '@angular/core';
 import {CommonModule} from '@angular/common';
 
 type PeriodType = 'week' | 'month' | 'current_year';
@@ -20,11 +19,34 @@ export class PeriodSelectorComponent {
   @Input() selectedPeriod: PeriodType = 'week';
   @Output() periodChange = new EventEmitter<PeriodType>();
 
+  isHidden = false;
   periods: Period[] = [
     {value: 'week', label: '7 derniers jours'},
     {value: 'month', label: '30 derniers jours'},
     {value: 'current_year', label: 'Depuis le 1er janvier'}
   ];
+  private lastScrollTop = 0;
+  private scrollThreshold = 50;
+
+  constructor(private elementRef: ElementRef) {
+  }
+
+  @HostListener('window:scroll', ['$event'])
+  onScroll() {
+    const currentScroll = window.scrollY;
+
+    if (currentScroll > this.scrollThreshold) {
+      if (currentScroll > this.lastScrollTop) {
+        this.isHidden = true; // Défilement vers le bas
+      } else {
+        this.isHidden = false; // Défilement vers le haut
+      }
+    } else {
+      this.isHidden = false; // Au début de la page
+    }
+
+    this.lastScrollTop = currentScroll;
+  }
 
   onChange(period: PeriodType) {
     this.selectedPeriod = period;
